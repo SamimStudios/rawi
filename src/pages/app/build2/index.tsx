d6a358d592a60dd747406c6bc07f5aebff0eeb1b
SamimStudios <finance@samimstudios.com>
2025-09-28 06:37:43 +0300

    Create index.tsx

---
+++ src/pages/app/build2/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import NodeLibraryBuilder2 from "@/features/node-builders2/NodeLibraryBuilder2";
+
+export default function Build2Page() {
+  return <NodeLibraryBuilder2 />;
+}
